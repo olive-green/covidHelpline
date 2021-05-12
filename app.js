b146069d163565f8ab5e4825204f5e84cfd917ca
olive-green <nareshbhairathore@gmail.com>
2021-05-12 15:19:39 +0530

    adding ejs files 2nd commit

--- app.js
+++ app.js
@@ -25,6 +25,27 @@ app.get("/adm",(req,res)=>{
     res.render("admin");
 })
 
+//hospitals page
+app.get("/hospitals",(req,res)=>{
+    let title="Hospitals";
+    res.render("hospitals",{title:title});
+})
+//plasma page
+app.get("/plasma",(req,res)=>{
+    let title="Plasma";
+    res.render("plasma",{title:title});
+})
+//hospitals page
+app.get("/bloodBank",(req,res)=>{
+    let title="Blood Bank"
+    res.render("bloodBank",{title:title});
+})
+//hospitals page
+app.get("/oxygen",(req,res)=>{
+    let title="Oxygen"
+    res.render("oxygen",{title:title});
+})
+
 //server connection
 app.listen(port,()=>{
     console.log(`server is listening on ${port}`);
